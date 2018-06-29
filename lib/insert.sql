3c2230c959cbcc98bfe7c390a8c5dcb7ccbccd6a
Marissa Zhong <mwz2101@columbia.edu>
2018-06-29 19:54:47 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,23 +1,23 @@
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
   VALUES ("Project 1", "Education", 100000, "01/01/2018", "01/01/2020");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-  VALUES ("Project 1", "Education", 100000, "01/01/2018", "01/01/2020");
+  VALUES ("Project 2", "Education", 100000, "01/01/2018", "01/01/2020");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-  VALUES ("Project 1", "Education", 100000, "01/01/2018", "01/01/2020");
+  VALUES ("Project 3", "Education", 100000, "01/01/2018", "01/01/2020");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-  VALUES ("Project 1", "Education", 100000, "01/01/2018", "01/01/2020");
+  VALUES ("Project 4", "Education", 100000, "01/01/2018", "01/01/2020");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-  VALUES ("Project 1", "Education", 100000, "01/01/2018", "01/01/2020");
+  VALUES ("Project 5", "Education", 100000, "01/01/2018", "01/01/2020");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-  VALUES ("Project 1", "Education", 100000, "01/01/2018", "01/01/2020");
+  VALUES ("Project 6", "Education", 100000, "01/01/2018", "01/01/2020");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-  VALUES ("Project 1", "Education", 100000, "01/01/2018", "01/01/2020");
+  VALUES ("Project 7", "Education", 100000, "01/01/2018", "01/01/2020");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-  VALUES ("Project 1", "Education", 100000, "01/01/2018", "01/01/2020");
+  VALUES ("Project 8", "Education", 100000, "01/01/2018", "01/01/2020");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-  VALUES ("Project 1", "Education", 100000, "01/01/2018", "01/01/2020");
+  VALUES ("Project 9", "Education", 100000, "01/01/2018", "01/01/2020");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-  VALUES ("Project 1", "Education", 100000, "01/01/2018", "01/01/2020");
+  VALUES ("Project 10", "Education", 100000, "01/01/2018", "01/01/2020");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
   VALUES ("Project 1", "Education", 100000, "01/01/2018", "01/01/2020");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)

d66e3b63920784d177cb3ab59526014915e7c2dd
Marissa Zhong <mwz2101@columbia.edu>
2018-06-29 19:54:55 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -18,7 +18,3 @@ INSERT INTO projects (title, category, funding_goal, start_date, end_date)
   VALUES ("Project 9", "Education", 100000, "01/01/2018", "01/01/2020");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
   VALUES ("Project 10", "Education", 100000, "01/01/2018", "01/01/2020");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-  VALUES ("Project 1", "Education", 100000, "01/01/2018", "01/01/2020");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-  VALUES ("Project 1", "Education", 100000, "01/01/2018", "01/01/2020");

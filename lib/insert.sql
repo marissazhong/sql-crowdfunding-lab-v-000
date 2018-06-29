5c5590ffb4801bbcef52e89c16a0b8c31911674a
Marissa Zhong <mwz2101@columbia.edu>
2018-06-29 19:55:22 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,18 +3,18 @@ INSERT INTO projects (title, category, funding_goal, start_date, end_date)
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
   VALUES ("Project 2", "Education", 100000, "01/01/2018", "01/01/2020");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-  VALUES ("Project 3", "Education", 100000, "01/01/2018", "01/01/2020");
+  VALUES ("Project 3", "Education", 200000, "01/01/2018", "01/01/2020");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-  VALUES ("Project 4", "Education", 100000, "01/01/2018", "01/01/2020");
+  VALUES ("Project 4", "Education", 200000, "01/01/2018", "01/01/2020");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-  VALUES ("Project 5", "Education", 100000, "01/01/2018", "01/01/2020");
+  VALUES ("Project 5", "Education", 300000, "01/01/2018", "01/01/2020");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-  VALUES ("Project 6", "Education", 100000, "01/01/2018", "01/01/2020");
+  VALUES ("Project 6", "Education", 300000, "01/01/2018", "01/01/2020");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-  VALUES ("Project 7", "Education", 100000, "01/01/2018", "01/01/2020");
+  VALUES ("Project 7", "Education", 400000, "01/01/2018", "01/01/2020");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-  VALUES ("Project 8", "Education", 100000, "01/01/2018", "01/01/2020");
+  VALUES ("Project 8", "Education", 400000, "01/01/2018", "01/01/2020");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-  VALUES ("Project 9", "Education", 100000, "01/01/2018", "01/01/2020");
+  VALUES ("Project 9", "Education", 500000, "01/01/2018", "01/01/2020");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-  VALUES ("Project 10", "Education", 100000, "01/01/2018", "01/01/2020");
+  VALUES ("Project 10", "Education", 500000, "01/01/2018", "01/01/2020");

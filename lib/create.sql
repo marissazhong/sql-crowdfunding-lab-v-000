6a77a5a94033c950cd31d24fdd6a0ea56cfbacd2
Marissa Zhong <mwz2101@columbia.edu>
2018-06-29 19:47:50 +0000

    automatically backed up by learn

---
+++ lib/create.sql
@@ -0,0 +1,16 @@
+CREATE TABLE projects (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal INTEGER,
+  start_date TEXT,
+  end_date TEXT
+);
+
+CREATE TABLE users (
+
+);
+
+CREATE TABLE pledges (
+
+);
